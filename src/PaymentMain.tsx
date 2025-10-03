972ab65ef7600fcd58b116cf3acb7d48364cf25c
Peter Thomas <peterglo2200@gmail.com>
2025-10-04 00:17:49 +0100

    Remove PaymentApp and PaymentMain components to streamline the payment handling process. This cleanup eliminates unused files and simplifies the application structure.

--- src/PaymentMain.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { createRoot } from "react-dom/client";
-import PaymentApp from "./PaymentApp.tsx";
-import "./index.css";
-
-const container = document.getElementById("payment-root");
-if (container) {
-  createRoot(container).render(<PaymentApp />);
-}
